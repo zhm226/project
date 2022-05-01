52360e33739822543a1c3388f91d10dccdc20c66
zhm226 <1720568892@qq.com>
2022-05-01 14:41:10 +0800

    commit four

--- student.java
+++ student.java
@@ -1,6 +1,7 @@
 import java.util.*
 class student{
-  private string name;
-  private string address;
-  private string email;   //修改的内容
+      private string name;
+      private string address;
+      private string email;
+      private String telephone;   //新增电话属性
 }
